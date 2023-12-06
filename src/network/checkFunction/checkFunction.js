cc75c1d37d692a0b7d5091e1155bcf7d2a5ac8c1
Sourav9063 <sourav.ahmed5654@gmail.com>
2023-12-06 21:28:41 +0600

    for  used

--- src/network/checkFunction/checkFunction.js
+++ src/network/checkFunction/checkFunction.js
@@ -24,4 +24,25 @@ export const getWorkingNotSureFromList = async ({
       });
     }
   });
+  // for (let i = 0; i < listOFLinks.length; i++) {
+  //   const link = listOFLinks[i];
+  //   try {
+  //     const response = await fetch(link, { mode: "no-cors" });
+
+  //     if (response.ok) {
+  //       console.log(link);
+  //       setWorking((state) => {
+  //         return [...state, { link: link, message: "Working" }];
+  //       });
+  //     } else {
+  //       setWorking((state) => {
+  //         return [...state, { link: link, message: "Working" }];
+  //       });
+  //     }
+  //   } catch (e) {
+  //     setNotSure((state) => {
+  //       return [...state, { link: link, message: "Not Sure" }];
+  //     });
+  //   }
+  // }
 };
