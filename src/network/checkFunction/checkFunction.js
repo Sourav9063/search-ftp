23eea532b0acdc8e03caeb77663ceb85e3b625c0
Sourav9063 <sourav.ahmed5654@gmail.com>
2024-01-02 16:11:43 +0600

    check func rewritten to add fvrt first

--- src/network/checkFunction/checkFunction.js
+++ src/network/checkFunction/checkFunction.js
@@ -4,64 +4,53 @@ export const getWorkingNotSureFromList = async ({
   listOFLinks,
   preferredKey = [],
 }) => {
-  //reverse preferredKey
-  let rearrangedList = [];
-  if (preferredKey.length === 0) {
-    rearrangedList = listOFLinks;
-  } else {
-    for (let link of listOFLinks) {
-      let isPreferred = false;
-      for (let key of preferredKey) {
-        if (link.includes(key)) {
-          rearrangedList = [link, ...rearrangedList];
-          isPreferred = true;
-          break;
-        }
-      }
-      if (!isPreferred) {
-        rearrangedList.push(link);
+  const preferredWorkingArray = [];
+  const notPreferredWorkingArray = [];
+  listOFLinks.forEach(async (link) => {
+    let isPreferred = false;
+    for (let key of preferredKey) {
+      if (link.includes(key)) {
+        isPreferred = true;
+        break;
       }
     }
-  }
-  listOFLinks.forEach(async (link) => {
-    try {
-      const response = await fetch(link, { mode: "no-cors" });
 
-      if (response.ok) {
-        console.log(link);
-        setWorking((state) => {
-          return [...state, { link: link, message: "Working" }];
-        });
+    try {
+      await fetch(link, { mode: "no-cors" });
+      if (isPreferred) {
+        preferredWorkingArray.push({ link: link, message: "Working" });
       } else {
-        setWorking((state) => {
-          return [...state, { link: link, message: "Working" }];
-        });
+        notPreferredWorkingArray.push({ link: link, message: "Working" });
       }
+      setWorking([...preferredWorkingArray, ...notPreferredWorkingArray]);
     } catch (e) {
       setNotSure((state) => {
         return [...state, { link: link, message: "Not Sure" }];
       });
     }
   });
-  // for (let i = 0; i < listOFLinks.length; i++) {
-  //   const link = listOFLinks[i];
-  //   try {
-  //     const response = await fetch(link, { mode: "no-cors" });
-
-  //     if (response.ok) {
-  //       console.log(link);
-  //       setWorking((state) => {
-  //         return [...state, { link: link, message: "Working" }];
-  //       });
-  //     } else {
-  //       setWorking((state) => {
-  //         return [...state, { link: link, message: "Working" }];
-  //       });
-  //     }
-  //   } catch (e) {
-  //     setNotSure((state) => {
-  //       return [...state, { link: link, message: "Not Sure" }];
-  //     });
-  //   }
-  // }
+  console.log(preferredWorkingArray.length);
+  console.log(notPreferredWorkingArray.length);
 };
+
+// for (let i = 0; i < listOFLinks.length; i++) {
+//   const link = listOFLinks[i];
+//   try {
+//     const response = await fetch(link, { mode: "no-cors" });
+
+//     if (response.ok) {
+//       console.log(link);
+//       setWorking((state) => {
+//         return [...state, { link: link, message: "Working" }];
+//       });
+//     } else {
+//       setWorking((state) => {
+//         return [...state, { link: link, message: "Working" }];
+//       });
+//     }
+//   } catch (e) {
+//     setNotSure((state) => {
+//       return [...state, { link: link, message: "Not Sure" }];
+//     });
+//   }
+// }
